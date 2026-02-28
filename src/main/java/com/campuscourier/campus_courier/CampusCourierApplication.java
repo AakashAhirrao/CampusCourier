440e79bb616c368e09a736925d0817640d247f3f
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-02-28 12:00:14 +0530

    Fresh start for CampusCourier

---
+++ src/main/java/com/campuscourier/campus_courier/CampusCourierApplication.java
@@ -0,0 +1,13 @@
+package com.campuscourier.campus_courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCourierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuscourier/campus_courier/CampusCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuscourier.campus_courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
