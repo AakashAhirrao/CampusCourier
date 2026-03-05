f7880a76b3ddbc349315f02781acfc57836dde94
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-03-06 02:19:56 +0530

    generated getters and setters

--- src/main/java/com/campuscourier/campus_courier/models/User.java
+++ src/main/java/com/campuscourier/campus_courier/models/User.java
@@ -24,4 +24,52 @@ public class User {
 
     @Column(name = "room_number", nullable = false, length = 20)
     private String roomNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHostelBlock() {
+        return hostelBlock;
+    }
+
+    public void setHostelBlock(String hostelBlock) {
+        this.hostelBlock = hostelBlock;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
