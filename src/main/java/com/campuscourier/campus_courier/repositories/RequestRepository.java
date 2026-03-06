c4306c6949f95cfc74b3ba8c882e028cc72da919
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-03-06 17:11:09 +0530

    new repository for handling requests

--- src/main/java/com/campuscourier/campus_courier/repositories/RequestRepository.java
+++ src/main/java/com/campuscourier/campus_courier/repositories/RequestRepository.java
@@ -4,6 +4,7 @@ import com.campuscourier.campus_courier.models.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Hibernate in java is responsible for translating java tags into actual raw SQL query
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 }
