9f5748edf83e49745d3b4afb65ae02df0a9a0c41
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-03-06 17:09:29 +0530

    RequestController for managing API for CC

---
+++ src/main/java/com/campuscourier/campus_courier/controllers/RequestController.java
@@ -0,0 +1,32 @@
+package com.campuscourier.campus_courier.controllers;
+
+import com.campuscourier.campus_courier.models.Request;
+import com.campuscourier.campus_courier.services.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+
+    private final RequestService requestService;
+
+    @Autowired
+    public RequestController (RequestService requestService){
+        this.requestService = requestService;
+    }
+
+    @PostMapping("/create/{userId}")
+    public Request createRequest (@PathVariable Long userId, @RequestBody Request newRequest){
+        // @PathVariable traps or catches the userId in curly braces converts it into the Long variable which is useful for seeing who
+        // is creating the request
+        return requestService.createRequest(userId, newRequest);
+    }
+
+    @GetMapping("/feed")
+    public List<Request> getFeed(){
+        return requestService.getAllRequests();
+    }
+}
