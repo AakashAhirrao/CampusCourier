2b84bab8ff956c1eb2d29f3e07d13de87b25db93
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-03-06 17:10:29 +0530

    Change in status saving, forcing PENDING as default. changing server for wrong ID

--- src/main/java/com/campuscourier/campus_courier/services/RequestService.java
+++ src/main/java/com/campuscourier/campus_courier/services/RequestService.java
@@ -5,7 +5,9 @@ import com.campuscourier.campus_courier.models.User;
 import com.campuscourier.campus_courier.repositories.RequestRepository;
 import com.campuscourier.campus_courier.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,7 +26,9 @@ public class RequestService {
     public Request createRequest (Long userId, Request newRequest) {
 
         User requester = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("Error: User with ID: "+userId+" does not exist"));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Error: User with ID: "+userId+" does not exist"));
         // this is part of what is called as Defensive programming. which uses wrapper called Optional
         // we told userRepository to find the user with ID, if it exists then good or .orElseThrow() methods acts
         // as a strict bouncer and throw a clean error message
@@ -32,6 +36,8 @@ public class RequestService {
         newRequest.setRequester(requester); // here we are taking newRequest object of Request class
         // and using the setRequest setter inside Request class to set requester
         // in simple terms we are setting User with given ID to given request
+
+        newRequest.setStatus("PENDING"); // setting status to PENDING as initial
 
         return requestRepository.save(newRequest);
     }
