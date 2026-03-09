4fa1c05382252a3f6ccbd57e1e818cdd31d125bf
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-03-09 18:41:04 +0530

    added findByStatus which uses JPA and hibernate to automatically write SQL queries

--- src/main/java/com/campuscourier/campus_courier/repositories/RequestRepository.java
+++ src/main/java/com/campuscourier/campus_courier/repositories/RequestRepository.java
@@ -4,7 +4,11 @@ import com.campuscourier.campus_courier.models.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // Hibernate in java is responsible for translating java tags into actual raw SQL query
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findByStatus(String status);
 }
