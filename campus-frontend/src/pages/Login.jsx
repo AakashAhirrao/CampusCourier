a2a18f2054ca99e8092b329c2f119ac64bdfeac9
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-04-04 12:40:26 +0530

    changes in frontend for deployment

--- campus-frontend/src/pages/Login.jsx
+++ campus-frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login(){
         console.log("Sending data to Spring Boot...");
 
         try{
-            const response = await fetch(`http://localhost:8080/api/auth/login`, {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
